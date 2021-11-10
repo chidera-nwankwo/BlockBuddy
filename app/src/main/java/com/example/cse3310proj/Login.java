5b8b8b1b99140e055fedd0392a74aebcee9f55f1
chidera-nwankwo <cnwankwo1234@yahoo.com>
2021-11-10 02:18:06 -0600

    UI fix and register error messages

--- app/src/main/java/com/example/cse3310proj/Login.java
+++ app/src/main/java/com/example/cse3310proj/Login.java
@@ -2,13 +2,29 @@ package com.example.cse3310proj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/cse3310proj/RegisterUser.java
+++ app/src/main/java/com/example/cse3310proj/RegisterUser.java
@@ -4,27 +4,94 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView register;
+    private  FirebaseAuth mAuth;
+
+    private TextView banner, registerUser;
+    private EditText editTextFullName, editTextAge, editTextEmail, editTextPassword;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
 
-        register = (TextView) findViewById(R.id.register);
-        register.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        banner = (TextView) findViewById(R.id.Title);
+        banner.setOnClickListener(this);
+
+        registerUser = (TextView) findViewById(R.id.registerUser);
+        registerUser.setOnClickListener(this);
+
+        editTextFullName = (EditText) findViewById(R.id.fullname);
+        editTextAge = (EditText) findViewById(R.id.age);
+        editTextEmail = (EditText) findViewById(R.id.Email);
+        editTextPassword = (EditText) findViewById(R.id.Password);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.register:
-                startActivity(new Intent(this, RegisterUser.class));
+            case R.id.Title:
+                startActivity(new Intent(this, MainActivity.class));
                 break;
+            case R.id.registerUser:
+                RegisterUser();
+                break;
+        }
+    }
+
+    private void RegisterUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String fullName = editTextFullName.getText().toString().trim();
+        String age = editTextAge.getText().toString().trim();
+
+        if(fullName.isEmpty()){
+            editTextFullName.setError("Full name is required.");
+            editTextFullName.requestFocus();
+            return;
+        }
+
+        if(age.isEmpty()){
+            editTextAge.setError("Age is required.");
+            editTextAge.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required.");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required.");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please provide valid Email.");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6) {
+            editTextPassword.setError("Password should be at least 6 characters.");
+            editTextPassword.requestFocus();
+            return;
         }
     }
 }
