81f4e0e48cfa4fb2cf879161db6e596cf79cd9b0
chidera-nwankwo <cnwankwo1234@yahoo.com>
2021-11-09 23:31:55 -0600

    Fixed Building error

--- app/src/main/java/com/example/cse3310proj/RegisterUser.java
+++ app/src/main/java/com/example/cse3310proj/RegisterUser.java
@@ -2,13 +2,29 @@ package com.example.cse3310proj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class RegisterUser extends AppCompatActivity {
+public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
     }
 }
